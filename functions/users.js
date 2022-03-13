d8121eaef6cde686b8924eab2fbfefcaad8aa9e6
Mohamed Gad <mohamedgad149@gmail.com>
2022-03-13 20:42:39 +0200

    final edits

--- functions/users.js
+++ functions/users.js
@@ -4,17 +4,24 @@ const db = admin.firestore();
 const users = express();
 
 const cors = require("cors");
-users.use(cors({origin: true}));
-
+users.use(cors({ origin: true }));
+users.options(
+  "*",
+  cors({
+    origin: true,
+    optionsSuccessStatus: 200,
+    credentials: true,
+  })
+);
 users.post("/", async (req, res) => {
   const user = req.body;
-  const result =  await db.collection("users").doc(user.uid).set(user);
+  const result = await db.collection("users").doc(user.uid).set(user);
   res.status(200).send(result);
 });
 
 users.get("/:id", async (req, res) => {
-    const id = req.params.id;
-    const user =  await db.collection("users").doc(id).get();
-    res.status(200).send(user.data());
-  });
+  const id = req.params.id;
+  const user = await db.collection("users").doc(id).get();
+  res.status(200).send(user.data);
+});
 module.exports = users;
