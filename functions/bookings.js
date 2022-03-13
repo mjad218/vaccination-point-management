6b7462bd15c7affb14ff87aec1a2fbdc678d2530
Mohamed Gad <mohamedgad149@gmail.com>
2022-03-13 14:42:20 +0200

    bookings post endpoint

--- functions/bookings.js
+++ functions/bookings.js
@@ -7,15 +7,6 @@ const cors = require("cors");
 
 bookings.use(cors({origin: true}));
 
-// bookings.get("/:id", async (req, res) => {
-//   const snapshot = await db.collection("users").doc(req.params.id).get();
-//   const user = {...snapshot.data()};
-//   if (!snapshot.exists) {
-//     res.status(404).send();
-//   }
-//   res.status(200).send(JSON.stringify(user));
-// });
-
 bookings.get("/", async (req, res) => {
     const snapshot = await db.collection("bookings").get();
     const bookings = [];
@@ -36,10 +27,10 @@ bookings.get("/:id", async (req, res) => {
    });
   res.status(200).send(JSON.stringify(bookings));
 });
-// bookings.post("/", async (req, res) => {
-//   const user = req.body;
-//   await db.collection("users").doc(`${user.blogID}`).set(user);
-//   res.status(200).send();
-// });
+bookings.post("/", async (req, res) => {
+  const user = req.body;
+  await db.collection("users").doc(`${user.blogID}`).set(user);
+  res.status(200).send();
+});
 
 module.exports = bookings;
